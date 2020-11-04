a478f4dd62b538c4781a7a059511f93f25c6a076
phoenixaustin81 <phoenixaustin1@gmail.com>
2020-11-03 19:47:24 -0600

    flask form back end (signup and login)

---
+++ forms.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=5, max=25)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password',
+                             validators=[DataRequired(), Length(min=5)])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=5, max=25)])
+    password = PasswordField('Password',
+                             validators=[DataRequired(), Length(min=5)])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
